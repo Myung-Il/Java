7cd86ab46823fb8b310ca9e48f40137e30b3576a
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-17 04:26:45 +0900

    백준 27889 해결

--- Main.java
+++ Main.java
@@ -3,19 +3,16 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt()+1;
-        int s1 = 0;
-        for (int i=1;i<x;i++){
-            s1 += i;
-        }
-        double s2 = Math.pow(s1, 2);
-        int s3 = 0;
-        for (double i=1;i<x;i++){
-            s3 += Math.pow(i, 3);
-        }
+        String n = sc.next();
 
-        System.out.println(s1);
-        System.out.println((int)s2);
-        System.out.println(s3);
+        if(n.equals("NLCS")){
+            System.out.println("North London Collegiate School");
+        }else if(n.equals("BHA")){
+            System.out.println("Branksome Hall Asia");
+        }else if(n.equals("KIS")){
+            System.out.println("Korea International School");
+        }else if(n.equals("SJA")){
+            System.out.println("St. Johnsbury Academy");
+        }
     }
 }
