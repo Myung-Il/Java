e3f918ed54c1fcdde22a612ba3996cf684b36187
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-19 16:48:27 +0900

    10992 브론즈 3 해결

---
+++ Foundation/Condition.java
@@ -0,0 +1,19 @@
+package Foundation;
+public class Condition {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        if (a<b){
+            System.out.println(true);
+        }
+        
+        // 중복 변수 안됨
+        String A = "희희";
+        String B = "희희";
+
+        if (A.equals(B)){
+            System.out.println(true);
+        }
+    }
+}
--- Main.java
+++ Main.java
@@ -4,9 +4,16 @@ public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt()+1;
+        int m = 1;
 
-        for (int i=1;i<n;i++){
-            System.out.println(" ".repeat(n-i-1)+"* ".repeat(i-1)+"*");
+        if (n>2){
+            System.out.println(" ".repeat(n-2)+"*");
         }
+        
+        for (int i=3;i<n;i++){
+            System.out.println(" ".repeat(n-i)+"*"+" ".repeat(m)+"*");
+            m+=2;
+        }
+        System.out.println("*".repeat(n*2-3));
     }
 }
