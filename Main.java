ea15b4c3be34da511796dba53fe0d0a493adb245
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-20 09:10:29 +0900

    5585 브론즈 2 해결

--- Main.java
+++ Main.java
@@ -3,17 +3,25 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt()+1;
-        int m = 1;
+        int n = 1000-sc.nextInt();
 
-        if (n>2){
-            System.out.println(" ".repeat(n-2)+"*");
+        int cnt = 0;
+        while (n!=0){
+            if (n>=500){
+                n-=500;
+            }else if (n>=100){
+                n-=100;
+            }else if (n>=50){
+                n-=50;
+            }else if (n>=10){
+                n-=10;
+            }else if (n>=5){
+                n-=5;
+            }else if (n>=1){
+                n-=1;
+            }
+            cnt+=1;
         }
-        
-        for (int i=3;i<n;i++){
-            System.out.println(" ".repeat(n-i)+"*"+" ".repeat(m)+"*");
-            m+=2;
-        }
-        System.out.println("*".repeat(n*2-3));
+        System.out.println(cnt);
     }
 }
