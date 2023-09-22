018a72e7584a69e63ca305521127d11acd380682
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-23 03:31:23 +0900

    2163 브론즈 1 도망친

--- Main.java
+++ Main.java
@@ -3,23 +3,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-
-        int n = s.length();
-        List<String> l = new ArrayList<>();
-        for (int i=1;i<n-1;i++){
-            for (int j=i+1;j<n;j++){
-                StringBuilder front = new StringBuilder(s.substring(0, i));
-                StringBuilder middle = new StringBuilder(s.substring(i, j));
-                StringBuilder back = new StringBuilder(s.substring(j));
-
-                String f_r = front.reverse().toString();
-                String m_r = middle.reverse().toString();
-                String b_r = back.reverse().toString();
-                l.add(f_r+m_r+b_r);
-            }
-        }
-        Collections.sort(l);
-        System.out.println(l.get(0));
+        int n, m;
+        n = sc.nextInt();
+        m = sc.nextInt();
+        System.out.println(n*m-1);
     }
 }
--- Room.java
+++ Room.java
@@ -1,13 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Room {
-    public static void main(String[] args) throws IOException {
-        int n = 5;
-        for (int i=1;i<n-1;i++){
-            for (int j=i+1;j<n;j++){
-                System.out.println(i+" "+j);
-            }
-        }
-    }
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int NM_arr[][] = new int[N][M];
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				NM_arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		int K = sc.nextInt();
+		int K_arr[][] = new int[K][4];
+		
+		for(int i = 0; i < K; i++) {
+			for(int j = 0; j < 4; j++) {
+				K_arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int l = 0; l < K; l++) {
+			int i = K_arr[l][0];
+			int j = K_arr[l][1];
+			int x = K_arr[l][2];
+			int y = K_arr[l][3];
+			int sum = 0;
+			int tmp = j;
+			
+			for(; i <= x; i++) {
+				for(; j <= y; j++)
+					sum += NM_arr[i-1][j-1];
+				j = tmp;
+			}
+			System.out.println(sum);
+		}
+	}
 }
