8d33b451da2b74928b6a4255bb9617d5aa9dee01
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-24 06:56:50 +0900

    13458 브론즈 2

--- Main.java
+++ Main.java
@@ -3,9 +3,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n, m;
+        int n;
         n = sc.nextInt();
-        m = sc.nextInt();
-        System.out.println(n*m-1);
+        int[] list = new int[n];
+        for (int i=0;i<n;i++){
+            list[i] = sc.nextInt();
+        }
+        int b,c;
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        long cnt = n;
+        for (int i=0;i<n;i++){
+            int af = list[i]-b;
+            if (af>0){
+                if (af%c>0){
+                    cnt++;
+                }
+                cnt += af/c;
+            }
+        }
+        System.out.println(cnt);
     }
 }
