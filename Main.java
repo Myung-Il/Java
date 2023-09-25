9ece39eab0bf9e87e3984ba9e564862ed85564c4
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-25 21:34:51 +0900

    9655 실버 5 너무 쉬웠던 문제

--- Main.java
+++ Main.java
@@ -3,26 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
-        n = sc.nextInt();
-        int[] list = new int[n];
-        for (int i=0;i<n;i++){
-            list[i] = sc.nextInt();
-        }
-        int b,c;
-        b = sc.nextInt();
-        c = sc.nextInt();
-
-        long cnt = n;
-        for (int i=0;i<n;i++){
-            int af = list[i]-b;
-            if (af>0){
-                if (af%c>0){
-                    cnt++;
-                }
-                cnt += af/c;
-            }
-        }
-        System.out.println(cnt);
+        int n = sc.nextInt();
+        System.out.println((n%2==1?"SK":"CY"));
     }
 }
