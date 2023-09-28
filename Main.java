8540f7eb3bc88a7e04ed2ebc9dd05c100004c0b1
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-09-28 18:56:40 +0900

    11728 실버 5

--- Main.java
+++ Main.java
@@ -3,15 +3,36 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        while (sc.hasNext()){
-            int s = 1,cnt = 1;
-            int n = sc.nextInt();
-            while ((s %= n)!=0){
-                s = s*10+1;
-                cnt++;
-            }
-            System.out.println(cnt);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        int[] la = new int[a];
+        for (int i=0;i<a;i++){
+            la[i] = sc.nextInt();
         }
+
+        int[] lb = new int[b];
+        for (int i=0;i<b;i++){
+            lb[i] = sc.nextInt();
+        }
+
+        Arrays.sort(la);
+        Arrays.sort(lb);
+        int n = 0,m = 0;
+        StringBuilder str = new StringBuilder();
+        while (n<a && m<b){
+            if (la[n]<lb[m]){
+                str.append(la[n++]+" ");
+            }else{
+                str.append(lb[m++]+" ");
+            }
+        }
+        while (n<a){
+            str.append(la[n++]+" ");
+        }while (m<b){
+            str.append(lb[m++]+" ");
+        }
+    System.out.println(str.toString());
     }
 }
--- Room.java
+++ Room.java
@@ -2,10 +2,7 @@ import java.util.*;
 
 public class Room {
 	public static void main(String[] args) {
-		// Scanner sc = new Scanner(System.in);
-		// int n = sc.nextInt();
-
-		int n = 111%9901;
-		System.out.println(n);
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
 	}
 }
