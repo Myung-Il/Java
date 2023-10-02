68e37828241840e6363ea7021ab3cb7fb1532445
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-10-03 00:07:21 +0900

    24416 브론즈 1

--- Main.java
+++ Main.java
@@ -1,16 +1,30 @@
 import java.util.*;
 
 public class Main {
+    static int a = 0;
+    static int b = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
-        int a,b;
-        for (int i=0;i<n;i++){
-            a = sc.nextInt();
-            b = sc.nextInt();
-            if (a<b)System.out.println("NO BRAINS");
-            else System.out.println("MMM BRAINS");
+
+        fib(n);
+        fibonacci(n);
+        System.out.println(a);
+        System.out.println(b);
+    }
+    public static int fib(int n) {
+        if (n==1 || n==2){a++;return 1;}
+        else return fib(n - 1) + fib(n - 2);
+    }
+    
+    public static int fibonacci(int n) {
+        int[] arr = new int[n+1]; 
+        arr[1] = arr[2] = 1; 
+
+        for(int i=3;i<=n;i++){
+            b++;
+            arr[i] = arr[i-1] + arr[i-2];
         }
+        return arr[n];
     }
 }
