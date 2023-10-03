cb78d7963b7de53bd09d290d3c16ceca9424208a
Myung-Il <92520262+Myung-Il@users.noreply.github.com>
2023-10-03 20:55:40 +0900

    1965 실버2

--- Main.java
+++ Main.java
@@ -1,30 +1,23 @@
 import java.util.*;
 
 public class Main {
-    static int a = 0;
-    static int b = 0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        fib(n);
-        fibonacci(n);
-        System.out.println(a);
-        System.out.println(b);
-    }
-    public static int fib(int n) {
-        if (n==1 || n==2){a++;return 1;}
-        else return fib(n - 1) + fib(n - 2);
-    }
-    
-    public static int fibonacci(int n) {
-        int[] arr = new int[n+1]; 
-        arr[1] = arr[2] = 1; 
-
-        for(int i=3;i<=n;i++){
-            b++;
-            arr[i] = arr[i-1] + arr[i-2];
+        int l[] = new int[n],dp[] = new int[n];
+        for (int i=0;i<n;i++)l[i] = sc.nextInt();
+        Arrays.fill(dp, 1);
+        int ans = 0;
+        
+        for(int i=1; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(l[i] > l[j]) {
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+            ans = Math.max(dp[i], ans);
         }
-        return arr[n];
+        System.out.println(ans);
     }
 }
