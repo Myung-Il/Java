36600a224c20584e5dc201a028e5031325c258b8
Myung-Il <ktr040415@gmail.com>
2023-10-11 10:03:14 +0900

    2914 브론즈 3

--- Room.java
+++ Room.java
@@ -3,21 +3,16 @@ import java.util.*;
 public class Room {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+		System.out.println("가위:0 바위:1 보:2");
+		int n = sc.nextInt();
 
-		int arr[] = new int[N],dp[] = new int[N];
-		for(int i=0; i<N; i++)arr[i] = sc.nextInt();
-		Arrays.fill(dp, 1);
-		int ans = 0;
-		
-		for(int i=1; i<N; i++) {
-			for(int j=0; j<i; j++) {
-				if(arr[i] > arr[j]) {
-					dp[i] = Math.max(dp[i], dp[j]+1); 
-				}
-			}
-			ans = Math.max(dp[i], ans);
-		}
-		System.out.println(ans);
+		int s = (int)(Math.random()*3);
+		String o = "";
+		if (n==0) {if (s==0) o = "com : 가위 /// 비김";if (s==1) o = "com : 가위 /// 졌음";if (s==2) o = "com : 가위 /// 이김";}
+		if (n==1) {if (s==0) o = "com : 바위 /// 이김";if (s==1) o = "com : 바위 /// 비김";if (s==2) o = "com : 바위 /// 졌음";}
+		if (n==2) {if (s==0) o = "com : 보 /// 졌음";if (s==1) o = "com : 보 /// 이김";if (s==2) o = "com : 보 /// 비김";}
+
+
+		System.out.println(o);
 	}
 }
