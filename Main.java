252f6c0f5b01a71a1be7fb83b59acb40caf64706
Myung-Il <ktr040415@gmail.com>
2023-10-19 16:53:00 +0900

    ++이 비교보다 우선 순위

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a, i;
-        a = sc.nextInt();
-        i = sc.nextInt();
-
-        System.out.println(a*(i-1)+1);
+    public static void main(String[] args) throws IOException {
+        // Scanner sc = new Scanner(System.in);
+        
     }
 }
