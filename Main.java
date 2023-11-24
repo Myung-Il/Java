9e39c9c1b3c9d7ad6ca61965aee8ad922f07be1f
Myung-Il <ktr040415@gmail.com>
2023-11-24 19:57:08 +0900

    2167 실버 5

--- Main.java
+++ Main.java
@@ -4,31 +4,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String s1 = sc.nextLine();
-        String s2 = sc.nextLine();
-        String s3 = sc.nextLine();
-
-        int a = s1.length()+1;
-        int b = s2.length()+1;
-        int c = s3.length()+1;
-
-        int[][][] arr = new int[a][b][c];
-        for(int i=1;i<a;i++){
-            for(int j=1;j<b;j++){
-                for(int k=1;k<c;k++){
-                    if(s1.charAt(i-1)==s2.charAt(j-1)&&s2.charAt(j-1)==s3.charAt(k-1))arr[i][j][k] = arr[i-1][j-1][k-1]+1;
-                    else{
-                        int[] l = {arr[i][j-1][k],arr[i][j][k-1]};
-                        int max = arr[i-1][j][k];
-                        for(int elm:l) if(elm>max) max = elm;
-                        arr[i][j][k] = max;
-                    }
-                }
+        int n = sc.nextInt()+1,m = sc.nextInt()+1;
+        int[][] list = new int[n][m];
+        for (int i=1;i<n;i++){
+            for (int j=1;j<m;j++){
+                list[i][j] = sc.nextInt();
             }
         }
-        System.out.println(arr[a-1][b-1][c-1]);
+
+        int k = sc.nextInt();
+        for (int cnt=0;cnt<k;cnt++){
+            int y1 = sc.nextInt(),x1 = sc.nextInt(),y2 = sc.nextInt(),x2 = sc.nextInt();
+            int sum = 0;
+
+            for (;y1<=y2;y1++){
+                for (int x=x1;x<=x2;x++){
+                    sum += list[y1][x];
+                }
+            }
+            System.out.println(sum);
+        }
     }
 }
-// abcdefghijklmn
-// bdefg
-// efg
+// 2 3
+// 1 2 4
+// 8 16 32
+// 3
+// 1 1 2 3
+// 1 2 1 2
+// 1 3 2 3
