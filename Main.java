6bc29bf80b69e00084de1c9a49c074db9f61b8b9
Myung-Il <ktr040415@gmail.com>
2023-11-28 22:26:15 +0900

    14916 실버 5

--- Main.java
+++ Main.java
@@ -4,32 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt()+1,m = sc.nextInt()+1;
-        int[][] list = new int[n][m];
-        for (int i=1;i<n;i++){
-            for (int j=1;j<m;j++){
-                list[i][j] = sc.nextInt();
+        int n = sc.nextInt();
+        int s = n/5;
+        if (n==1 || n==3)System.out.println(-1);
+        else{
+            switch (n%5) {
+                case 0:System.out.println(s);break;
+                case 1:System.out.println(s+2);break;
+                case 2:System.out.println(s+1);break;
+                case 3:System.out.println(s+3);break;
+                case 4:System.out.println(s+2);break;
             }
-        }
-
-        int k = sc.nextInt();
-        for (int cnt=0;cnt<k;cnt++){
-            int y1 = sc.nextInt(),x1 = sc.nextInt(),y2 = sc.nextInt(),x2 = sc.nextInt();
-            int sum = 0;
-
-            for (;y1<=y2;y1++){
-                for (int x=x1;x<=x2;x++){
-                    sum += list[y1][x];
-                }
-            }
-            System.out.println(sum);
         }
     }
 }
-// 2 3
-// 1 2 4
-// 8 16 32
-// 3
-// 1 1 2 3
-// 1 2 1 2
-// 1 3 2 3
