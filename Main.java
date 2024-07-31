800e9f5b005e9c8e9f7740f663c01fd406a0a205
Myung-Il <ktr040415@gmail.com>
2024-08-01 02:02:39 +0900

    골드 3 - X
    재귀적인 특징을 가진다는 것은 풀면서 알았지만 DFS를 이용해서 풀 수 있을 것이라고는 생각 못했다, 규칙도 찾아서 풀었다고 생각했는데 조금 더 깊게 생각하지 못해서 아쉽다

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
-package Samsung_A_previous_exam;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 
-public class G16637 {
+public class Main {
     static int ans = Integer.MIN_VALUE;
     static ArrayList<Integer> num;
     static ArrayList<Character> symbol;
@@ -12,13 +10,13 @@ public class G16637 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.nextLine();            // 개행문자 처리
+        sc.nextLine();
         String s = sc.nextLine();
 
         num = new ArrayList<>();
         symbol = new ArrayList<>();
 
-        for (int i=0; i<n; i++) { // 숫자와 연산자 구분
+        for (int i=0; i<n; i++) {
             if (i % 2 == 0)num.add(Character.getNumericValue(s.charAt(i)));
             else symbol.add(s.charAt(i));
         }
@@ -28,7 +26,6 @@ public class G16637 {
 
     }
         
-    // 연산자에 따른 계산
     public static int operation(char op, int a, int b){
         switch (op) {
             case '+':return a + b;
@@ -36,15 +33,13 @@ public class G16637 {
             default: return a * b;
         }
     }
-
-    // 규칙
     public static void dfs(int deep, int sum){
-        if (deep==symbol.size()){ // 최대 계산 횟수는 연산자의 수만큼
+        if (deep==symbol.size()){
             ans = Math.max(ans, sum);
             return;
         }
-        dfs(deep+1, operation(symbol.get(deep), sum, num.get(deep+1)));           // 이전 합과 현재의 값을 계산하고 다음으로 넘김
-        if (deep+2 <= symbol.size()) dfs(deep+2, operation(symbol.get(deep), sum, // 괄호 계산 부분
-            operation(symbol.get(deep+1), num.get(deep+1), num.get(deep+2))));    // 두 수를 먼저 계산한 다음 이전 합과 계산한다
+        dfs(deep+1, operation(symbol.get(deep), sum, num.get(deep+1)));
+        if (deep+2 <= symbol.size()) dfs(deep+2, operation(symbol.get(deep), sum,
+            operation(symbol.get(deep+1), num.get(deep+1), num.get(deep+2))));
     }
 }
