fc3ce6becd49b00127cf762995976d36657fee6c
Myung-Il <ktr040415@gmail.com>
2024-07-31 23:16:05 +0900

    자바 공부 셋팅
    VScode로 작성하는거라 좀 파일이 이상할 수 있음
    ecliepse로 할 생각이였는데 Java가 이미 있어서..그냥 하기로 함

---
+++ Test1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class List{
+    public void make(int n, int m, int list[][]){
+
+    }
+}
+
+public class Test1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] list1 = new int[n][m];
+        for(int i=-1;i++<n;){
+            for(int j=-1;j++<m;){
+                list1[j][i] = sc.nextInt();
+            }
+        }
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[][] list2 = new int[n][m];
+        for(int i=-1;i++<n;){
+            for(int j=-1;j++<m;){
+                list2[b][a] = sc.nextInt();
+            }
+        }
+
+
+    }
+}
---
+++ 삼성A형기출문제/_16637_괄호추가하기.java
@@ -0,0 +1,7 @@
+package 삼성A형기출문제;
+
+public class _16637_괄호추가하기 {
+    public static void Main(){
+        
+    }
+}
